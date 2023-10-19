19c9f185e9057b57f8322763a68d114bee912c9c
Zakeria Ali <zak.fined@gmail.com>
2023-10-19 09:03:00 +0100

    Add book page hero

--- gatsby-theme/src/components/ImageWrapper.js
+++ gatsby-theme/src/components/ImageWrapper.js
@@ -1,9 +1,10 @@
 import React from "react"
 import "../styles/hero-images.scss"
 
-function Image({children}){
+function Image({children, type, direction = 'top'}){
+    const imageBorders = type === 'ceil' ? (direction === 'left' ? 'image__item--ceil-left' : 'image__item--ceil-top') : 'image__item-circle' 
     return (
-        <div className="hero-image-wrapper__item">
+        <div className={`image__item ${imageBorders}`}>
             {children}
         </div>
     )
@@ -12,13 +13,17 @@ function Image({children}){
 export default function ImageWrapper({type, children}){
     if(type === 'multiple'){
         return (
-            <div className="hero-image-wrapper">
+            <div className="hero-images-wrapper">
                 {children.map((child) => (
                     <Image type="ceil">{child}</Image>
                 ))}
             </div>
         )
     } else {
-        return null
+        return (
+            <div className="hero-image-wrapper">
+                <Image type="ceil" direction="left">{children}</Image>
+            </div> 
+        )
     }
 }
