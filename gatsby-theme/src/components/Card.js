ed95dd3c082be3159354447c0c7a88ae512c8c72
Zakeria Ali <zak.fined@gmail.com>
2023-12-09 20:24:17 +0000

    Edit netlify config and fix error in card comp

--- gatsby-theme/src/components/Card.js
+++ gatsby-theme/src/components/Card.js
@@ -4,6 +4,7 @@ import RatingStar from "./RatingStar"
 import "../styles/card.scss"
 
 export default function Card({book, image, hasImageDisplacement = true, layout = 'simple', roundedCorners = true}){
+    console.log(book)
     return (
         <Link className={`card ${!hasImageDisplacement ? 'card--no-displacement' : ''} ${layout === 'complex' ? 'card--complex' : ''} ${roundedCorners ? 'card--with-rounded-corners' : ''}`} to={`/books/${book.slug}`}>
             <div className="card__image">
@@ -11,7 +12,7 @@ export default function Card({book, image, hasImageDisplacement = true, layout =
             </div>
             <div className="card__content">
                 <h3>{book.title}</h3>
-                <p>By {book.authors[0].name}</p>
+                {book.authors.length && <p>By {book.authors[0].name}</p>}
                 <div className="card__content-category"><span>{book.category}</span></div>
                 <div className="card__content-ratings">
                     <RatingStar rating={book.averageRating} />
