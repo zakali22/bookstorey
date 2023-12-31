bba8113606428272fb899c5548360279c5ab91f9
Zakeria Ali <zak.fined@gmail.com>
2023-12-31 20:12:41 +0000

    Add netlify form submission

--- gatsby-theme/src/components/Footer.js
+++ gatsby-theme/src/components/Footer.js
@@ -1,27 +1,51 @@
-import React from "react"
+import React, { useState } from "react"
 import "../styles/footer.scss"
 import Container from "./Container"
 import Button from "./Button"
 import { Link } from "gatsby"
 import {useTheme} from "../../../site/src/utils/theme"
 import {useAuth} from "../../../site/src/utils/auth"
+import toast from "react-hot-toast"
 
 export default function Footer(){
     const {darkMode} = useTheme()
     const {currentUser} = useAuth()
+    const [formSubmitted, setFormSubmitted] = useState(false)
+
+    function handleFormSubmit(e){
+        e.preventDefault()
+
+        const formData = new FormData(e.target)
+
+        fetch("/", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: new URLSearchParams(formData).toString(),
+        })
+        .then(() => {
+            setFormSubmitted(true)
+            toast.success("Form successfully submitted")
+        })
+        .catch((error) => alert(error));
+    }
 
     return (
         <footer className="footer">
             <Container>
                 <div className="footer__left">
                     <div className="footer__subscription">
-                        <h4>Subscribe to the Bookstorey newsletter to receive the latest books</h4>
-                        <form>
-                            <label>
-                                <input />
-                                <Button type="tertiary" inverted={darkMode}>Sign me up</Button>
-                            </label>
-                        </form>
+                        {formSubmitted && <h4>Thank you for subscribing to the Bookstorey newsletter</h4>}
+                        {!formSubmitted && (
+                            <>
+                                <h4>Subscribe to the Bookstorey newsletter to receive the latest books</h4>
+                                <form name="newsletter" method="POST" data-netlify="true" onSubmit={handleFormSubmit}>
+                                    <label>
+                                        <input type="email" name="email" required/>
+                                        <Button type="tertiary" buttonType="submit" inverted={darkMode}>Sign me up</Button>
+                                    </label>
+                                </form>
+                            </>
+                        )}
                     </div>
                 </div>
 
